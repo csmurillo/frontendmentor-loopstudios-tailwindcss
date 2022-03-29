7c731edf35c7adaa9961b7361ff154574986aed5
csmurillo <csmurillo00@gmail.com>
2022-03-28 22:56:46 -0700

    done.

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static getInitialProps ({ renderPage }) {   
+    return renderPage();
+  }
+
+  render () {    
+    return (
+      <html>
+        <Head>
+          <title>Document</title>
+          <link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet"></link>
+            <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap" rel="stylesheet"></link>
+        </Head>        
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
